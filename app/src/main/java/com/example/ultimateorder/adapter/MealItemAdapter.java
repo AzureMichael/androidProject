94a7d166a7a00e1c46b893e7b86297354981b0c2
Stefanescu Mihai <mihailovici111@gmail.com>
2019-12-06 00:09:58 +0200

    finished add operation for orders

--- app/src/main/java/com/example/ultimateorder/adapter/MealItemAdapter.java
+++ app/src/main/java/com/example/ultimateorder/adapter/MealItemAdapter.java
@@ -1,10 +1,12 @@
 package com.example.ultimateorder.adapter;
 
 import android.content.Context;
+import android.database.MatrixCursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.ultimateorder.R;
@@ -12,11 +14,13 @@ import com.example.ultimateorder.model.MealItem;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MealItemAdapter extends ArrayAdapter<MealItem> implements View.OnClickListener {
 
     private ArrayList<MealItem> mealItems;
     Context mContext;
+    public List<MealItem> newOrder = new ArrayList<>();
 
     public MealItemAdapter(ArrayList<MealItem> data, Context context) {
         super(context, R.layout.meal_item_layout, data);
@@ -38,8 +42,6 @@ public class MealItemAdapter extends ArrayAdapter<MealItem> implements View.OnCl
         }
     }
 
-    private int lastPosition = -1;
-
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View listItem = convertView;
@@ -56,6 +58,16 @@ public class MealItemAdapter extends ArrayAdapter<MealItem> implements View.OnCl
 
         TextView price = (TextView) listItem.findViewById(R.id.price);
         price.setText(String.valueOf(currentM.getPrice()));
+
+        Button addButton = listItem.findViewById(R.id.addToOrderButton);
+        View finalListItem = listItem;
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newOrder.add(currentM);
+            }
+        });
+
 
         return listItem;
     }
